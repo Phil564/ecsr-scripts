495a02a7f1f9c637b0cbe3c9a11b16a1ae8e919a
Phil564 <93886661+Phil564@users.noreply.github.com>
2023-04-23 15:19:35 -0400

    Update colorfixMODERNside.lua

--- colorfixMODERNside.lua
+++ colorfixMODERNside.lua
@@ -1,7 +1,7 @@
 local function scan(parent)
 	for _,v in pairs(parent:GetChildren()) do
 		if v.BrickColor then
-			if not v:FindFirstChild("BrickColorValue") or not v.ClassName = "BrickColorValue" then
+			if not v:FindFirstChild("BrickColorValue") or not v.ClassName == "BrickColorValue" then
 				local ogpartcolor = Instance.new("BrickColorValue")
 				ogpartcolor.Name = "BrickColorValue"
 				ogpartcolor.Value = v.BrickColor
