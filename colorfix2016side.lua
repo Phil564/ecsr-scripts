49f26dc963335941015bf582d2c44181a79cb38b
Phil564 <93886661+Phil564@users.noreply.github.com>
2023-04-23 15:13:55 -0400

    updated scripts to support a wider range of parts

--- colorfix2016side.lua
+++ colorfix2016side.lua
@@ -1,7 +1,7 @@
 local function scan(parent)
 	for _,v in pairs(parent:GetChildren()) do
-		if v.ClassName == "Part" or v.ClassName == "MeshPart" or v.ClassName == "Wedge" or v.ClassName == "CornerWedge" or v.ClassName == "Union" then
-			if v:FindFirstChild("BrickColorValue") then
+		if v.BrickColor and v:FindFirstChild("BrickColorValue") then
+			if not v.ClassName = "BrickColorValue" then
 				v.BrickColor = v.BrickColorValue.Value
 				v.BrickColorValue:Destroy()
 			end
--- colorfixMODERNside.lua
+++ colorfixMODERNside.lua
@@ -1,7 +1,7 @@
 local function scan(parent)
 	for _,v in pairs(parent:GetChildren()) do
-		if v.ClassName == "Part" or v.ClassName == "MeshPart" or v.ClassName == "Wedge" or v.ClassName == "CornerWedge" or v.ClassName == "Union" then
-			if not v:FindFirstChild("BrickColorValue") then
+		if v.BrickColor then
+			if not v:FindFirstChild("BrickColorValue") or not v.ClassName = "BrickColorValue" then
 				local ogpartcolor = Instance.new("BrickColorValue")
 				ogpartcolor.Name = "BrickColorValue"
 				ogpartcolor.Value = v.BrickColor
